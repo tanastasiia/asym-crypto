1d7cb117da53160ddd593460d2b66dd7a1cc15f4
VenomNK <nikitakorzh10@gmail.com>
2020-09-16 22:51:16 +0300

    L89

---
+++ src/main/java/ua/kpi/generators/L89.java
@@ -0,0 +1,10 @@
+package ua.kpi.generators;
+
+import java.math.BigInteger;
+
+public class L89 implements Generator {
+    @Override
+    public BigInteger generate(int length) {
+        return null;
+    }
+}
