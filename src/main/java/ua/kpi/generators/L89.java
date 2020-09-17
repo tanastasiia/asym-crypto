f8de462ab3ca864c5a4f42ed128664b22736e480
tanastasiia <anastasia.taftay@gmail.com>
2020-09-17 20:42:26 +0300

    comments edit

--- src/main/java/ua/kpi/generators/L89.java
+++ src/main/java/ua/kpi/generators/L89.java
@@ -4,8 +4,8 @@ import java.math.BigInteger;
 
 public class L89 implements Generator {
 
-    // long - 63 бита хранит а надо 89 => использовать BigInteger!!!!
-    //построить новый LFSR на BigInteger а не long
+    // long - 63 бита хранит а надо 89 => использовать массив из двух лонгов или BigInteger!!!!
+    //построить новый LFSR на BigInteger или массиве из двух лонгов а не long
     @Override
     public String generate(int length) {
         return null;
