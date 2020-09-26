56ec3b6348f1ce9e72e1d9af217cc4dc7dfa294e
VenomNK <nikitakorzh10@gmail.com>
2020-09-26 20:26:49 +0300

    LehmerLow
    Генератор псевдопростых чисел LehmerLow

--- src/main/java/ua/kpi/generators/LehmerLow.java
+++ src/main/java/ua/kpi/generators/LehmerLow.java
@@ -1,5 +1,7 @@
 package ua.kpi.generators;
 
+import ua.kpi.generators.util.Util;
+
 import java.math.BigInteger;
 
 public class LehmerLow extends Lehmer {
@@ -8,6 +10,16 @@ public class LehmerLow extends Lehmer {
 
     @Override
     public String generate(int bitLength) {
-        return null;
+        int byteLength = bitLength / 8 + (bitLength % 8 != 0 ? 1 : 0);
+
+        long x = Util.random(maskM);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < byteLength; i++) {
+            x = (a * x + c) & maskM;
+            sb.append(Util.makeBinaryString(x & 0xFF , 8));
+            //sb.append(Util.makeBinaryString((x & mask8lowBits) >> 8 , 8));
+        }
+        return sb.substring(0, bitLength);
+
     }
 }
