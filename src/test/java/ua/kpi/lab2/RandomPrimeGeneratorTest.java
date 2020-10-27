94c9c8c82b9f50c5c68096a91ec0d34973102877
tanastasiia <anastasia.taftay@gmail.com>
2020-10-27 18:43:20 +0200

    some tests defined

---
+++ src/test/java/ua/kpi/lab2/RandomPrimeGeneratorTest.java
@@ -0,0 +1,17 @@
+package ua.kpi.lab2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class RandomPrimeGeneratorTest {
+    @Test
+    public void millerRabinRandom(){
+        //generate compare miller rabin with isProbablePrime from BigInteger in cycle for 100 times
+    }
+
+    @Test
+    public void millerRabinPrimeTrue(){
+        //google prime number(s) and test
+        //use assertTrue() from junit
+    }
+}
