8d85adb3fcfa854e0997209e0ba8de1e588e385b
VenomNK <nikitakorzh10@gmail.com>
2020-11-02 17:35:59 +0200

    RandomPrimeGenerator

--- src/main/java/ua/kpi/lab2/RandomPrimeGenerator.java
+++ src/main/java/ua/kpi/lab2/RandomPrimeGenerator.java
@@ -9,16 +9,28 @@ public class RandomPrimeGenerator {
 
     private Generator gen = new L89();
     private final MillerRabinPrimalityTest mrTest = new MillerRabinPrimalityTest();
+    private Object MillerRabinPrimalityTest;
 
     public RandomPrimeGenerator(Generator gen) {
         this.gen = gen;
     }
 
-    public RandomPrimeGenerator() {
+    public RandomPrimeGenerator(){}
 
-    }
+    public BigInteger generatePrime(int byteLength) {
+        BigInteger TWO = BigInteger.valueOf(2);
 
-    public BigInteger generatePrime(int bitLength) {
-        throw new UnsupportedOperationException();
+        int[] generate = gen.generate(byteLength);
+        byte[] b = new byte[byteLength];
+
+        for (int i = 0; i < generate.length; i++) {
+            b[i] = (byte) generate[i];
+        }
+
+        BigInteger m = new BigInteger(b);
+        while (!mrTest.test(m)) {
+            m = m.add(TWO);
+        }
+        return m;
     }
 }
